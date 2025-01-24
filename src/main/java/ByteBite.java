59f4a2b56a80da19c7b0cec5685c2d405b0970e7
DESU-CLUB <warrenlowkh@gmail.com>
2025-01-25 01:00:59 +0800

    Add functionality to add and list commands

--- src/main/java/ByteBite.java
+++ src/main/java/ByteBite.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class ByteBite {
    private static final String LOGO = """
@@ -13,6 +14,8 @@ public class ByteBite {
    private static final String ANSI_CYAN = "\u001B[36m";
    private static final String ANSI_RESET = "\u001B[0m";
    private static final String BORDER = "─".repeat(50);
+   private ArrayList<String> tasks = new ArrayList<>();
+
    private static final String FAREWELL = """
        🤖 *beep* *boop* 
        Powering down... Hope to see you again soon! 
@@ -34,13 +37,27 @@ public class ByteBite {
            while ((input = reader.readLine()) != null) {
                if (input.equalsIgnoreCase("bye")) {
                   printWithBorder(ANSI_CYAN + FAREWELL + ANSI_RESET);
-                   break;
+                  break;
                }
-               printWithBorder("\t"+input);
+               else if (input.equalsIgnoreCase("list")){
+                 showList();
+               }
+               else {
+                 tasks.add(input);
+                 printWithBorder("\tadded: "+input);
+               }
            }
        } catch (Exception e) {
            System.err.println("Error reading input: " + e.getMessage());
        }
+   }
+
+   private void showList() {
+       StringBuilder list = new StringBuilder();
+       for (int i = 0; i < tasks.size(); i++) {
+           list.append(String.format("%d. %s%n", i + 1, tasks.get(i)));
+       }
+       printWithBorder(list.toString().trim());
    }
 
    private void printWithBorder(String message) {
